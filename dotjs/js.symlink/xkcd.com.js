1848f44cc64634267af8dfe5faf9c709abc8cfc5
Jacob Gillespie <mail@jacobwg.com>
2012-08-25 11:33:41 -0500

    Added dotjs support

---
+++ dotjs/js.symlink/xkcd.com.js
@@ -0,0 +1,5 @@
+$('#transcript').html("<p>" + $('#comic img').attr('title') + "</p>")
+                .css('display', 'block')
+                .css('font-size', '110%')
+                .css('font-variant', 'normal')
+                .css('padding', '10px 20px');
