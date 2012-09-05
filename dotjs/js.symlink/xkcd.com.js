bb18541d4c293aa6972b701d26b7116f13a9bfa5
Jacob Gillespie <mail@jacobwg.com>
2012-09-04 20:12:19 -0500

    Removing dotjs (not using anymore)

--- dotjs/js.symlink/xkcd.com.js
+++
@@ -1,5 +0,0 @@
-$('#transcript').html("<p>" + $('#comic img').attr('title') + "</p>")
-                .css('display', 'block')
-                .css('font-size', '110%')
-                .css('font-variant', 'normal')
-                .css('padding', '10px 20px');
--- dotjs/js.symlink/youtube.com.js
+++
@@ -1,8 +0,0 @@
-// Attempt to stop auto-play on YouTube videos - currently broken
-
-var player = $('#movie_player');
-
-if (!player) return;
-
-player.attr('flashvars', "autoplay=0&" + player.attr('flashvars'));
-player.attr('src', player.attr('src') + "#");
