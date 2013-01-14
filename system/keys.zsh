783d2e64481fd65598a6b3d1267daa5ee3f1cf59
Jacob Gillespie <mail@jacobwg.com>
2013-01-14 10:50:59 -0600

    Remove old hosts alias

--- system/keys.zsh
+++ system/keys.zsh
@@ -1,7 +1,3 @@
-# Remove the hosts that I don't want to keep around- in this case, only
-# keep the first host. Like a boss.
-alias hosts="head -2 ~/.ssh/known_hosts | tail -1 > ~/.ssh/known_hosts"
-
 # Pipe my public key to my clipboard.
 alias pubkey="more ~/.ssh/id_dsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 
