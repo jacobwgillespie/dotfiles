6d1082688c58ebabdcf245d1e3488f1b81a078a7
Jacob Gillespie <mail@jacobwg.com>
2013-06-05 15:40:21 -0500

    Update to system stuff

--- system/keys.zsh
+++ system/keys.zsh
@@ -1,6 +1,2 @@
 # Pipe my public key to my clipboard.
 alias pubkey="more ~/.ssh/id_dsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
-
-# Generate pubkey
-# TODO: make this into a proper script, asking for email
-alias genpubkey="ssh-keygen -t dsa -C 'mail@jacobwg.com'"
