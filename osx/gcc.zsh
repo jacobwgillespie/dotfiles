db181719618bc37f1fb17dcd825602189a92107d
Jacob Gillespie <jacobwgillespie@gmail.com>
2013-12-06 20:14:50 -0600

    Remove unnecessary stuff, only source rbenv if installed

--- osx/gcc.zsh
+++
@@ -1,9 +0,0 @@
-# TODO: find out if we need this
-
-# gcc on lion
-export CC=/usr/bin/gcc
-
-# This might be because I have command line tools only
-# and not Xcode
-
-# export CC=/usr/bin/gcc-4.2
--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,4 +1,4 @@
-# Now using ruby-1.9.3-p194
-alias r='rbenv local 1.9.3-p194'
+# Now using ruby-2.0.0-p353
+alias r='rbenv local 2.0.0-p353'
 
 alias migrate='rake db:migrate db:test:clone'
