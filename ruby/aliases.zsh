98a66db3c9ce34fb6220bbf35c89188e8d04923b
Jacob Gillespie <jacobwgillespie@gmail.com>
2014-02-27 16:29:40 -0600

    Clean up ruby aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,10 +1,3 @@
-# Now using ruby-2.0.0-p353
-#alias r='rbenv local 2.0.0-p353'
-
-alias migrate='rake db:migrate db:test:clone'
-
-spring_path=$(which spring)
-if (( $+commands[spring] ))
-then
-  alias r="$spring_path rails"
-fi
+alias sc='script/console'
+alias sg='script/generate'
+alias sd='script/destroy'
