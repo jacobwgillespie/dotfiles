c1001058fa5294e677a2b2e703056deffcc02928
Jacob Gillespie <jacobwgillespie@gmail.com>
2016-11-21 18:35:55 -0600

    Add gnew alias for work

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,3 +17,5 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+
+alias gnew="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative HEAD..dev"
