4c492498cc5cc7514aa240137cc06e62acaf99cd
Jacob Gillespie <jacobwgillespie@gmail.com>
2017-02-15 13:24:37 -0600

    Add aliases for standard ls

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -6,5 +6,10 @@ then
   alias ls="gls -F --color"
   alias l="gls -lAh --color"
   alias ll="gls -l --color"
-  alias la='gls -A --color'
+  alias la="gls -A --color"
+else
+  alias ls="ls -F --color"
+  alias l="ls -lAh --color"
+  alias ll="ls -l --color"
+  alias la="ls -A --color"
 fi
