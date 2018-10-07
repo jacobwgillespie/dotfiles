f398ff765251745b0f3f6613a62d8086615445c4
Jacob Gillespie <jacobwgillespie@gmail.com>
2018-10-06 21:41:44 -0500

    Set up completion for GCP CLI

---
+++ k8s/google.zsh
@@ -0,0 +1,4 @@
+if [ -d /usr/local/Caskroom/google-cloud-sdk ]; then
+  source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
+  source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
+fi
