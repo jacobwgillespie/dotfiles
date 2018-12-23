eb0c43b8d7bac20c02c1e03577df32a6f3777446
Jacob Gillespie <jacobwgillespie@gmail.com>
2018-12-23 00:06:51 -0600

    Fix path ordering for Node

--- config/path.zsh
+++
@@ -1,2 +0,0 @@
-export PATH="./bin:$HOME/bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:/sbin"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
