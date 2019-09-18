fc2e7e4b5bd4d191cca76326f35880ae2a724641
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-09-18 14:59:02 +0100

    Remove Ruby config

--- config/ruby.zsh
+++
@@ -1,8 +0,0 @@
-if (( $+commands[rbenv] )); then
-  eval "$(rbenv init -)"
-else
-  if [ -d "$HOME/.rbenv/bin" ]; then
-    export PATH="$HOME/.rbenv/bin:$PATH"
-    eval "$(rbenv init -)"
-  fi
-fi
