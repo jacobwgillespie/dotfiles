6b13149864f58de3f023365776a707346d050fd0
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-12-17 00:55:00 +0000

    Add exa and bat utilities

--- config/coreutils.zsh
+++ config/coreutils.zsh
@@ -1,19 +1,27 @@
 # Colorize common tools with grc
-if (( $+commands[grc] )) && (( $+commands[brew] ))
-then
+if (( $+commands[grc] )) && (( $+commands[brew] )); then
   source `brew --prefix`/etc/grc.bashrc
 fi
 
-# Override ls with gls, add colors
-if $(gls &>/dev/null)
-then
+# Override ls with exa or gls, add colors
+if (( $+commands[exa] )); then
+  alias ls="exa -F"
+  alias l="exa -la"
+  alias ll="exa -l"
+  alias la="exa -a"
+elif (( $+commands[gls] )); then
   alias ls="gls -F --color"
   alias l="gls -lAh --color"
   alias ll="gls -l --color"
   alias la="gls -A --color"
 else
-  alias ls="ls -F --color"
-  alias l="ls -lAh --color"
-  alias ll="ls -l --color"
-  alias la="ls -A --color"
+  alias ls="ls -F"
+  alias l="ls -lAh"
+  alias ll="ls -l"
+  alias la="ls -A"
+fi
+
+# Override cat with bat
+if (( $+commands[bat] )); then
+  alias cat=bat
 fi
