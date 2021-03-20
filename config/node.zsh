1e924fe31b5b507e67b68a0b6b3828b132748eb2
Jacob Gillespie <jacobwgillespie@gmail.com>
2021-03-20 00:20:52 +0000

    Set up dev CLI completion after node

--- config/node.zsh
+++ config/node.zsh
@@ -4,3 +4,10 @@ fi
 
 export N_PREFIX="$HOME/.n"
 export PATH="$N_PREFIX/bin:$PATH"
+
+# This is a private CLI, you can ignore this file
+if (( $+commands[dev] )); then
+  source <(dev completion script)
+  alias d=dev
+  compdef d=dev
+fi
