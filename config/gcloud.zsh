847205c2c8182261921f7c1d2fece61fea81e042
Jacob Gillespie <jacobwgillespie@gmail.com>
2021-07-21 17:03:13 +0100

    Add gcloud support

---
+++ config/gcloud.zsh
@@ -0,0 +1,4 @@
+if [ -f /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc ]; then
+  source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+  source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+fi
