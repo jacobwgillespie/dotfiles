9cbf0845acb468964179e643bef5c8d338a17b12
Jacob Gillespie <jacobwgillespie@gmail.com>
2021-08-28 22:26:14 +0100

    Add Rust version to iTerm, fix display

--- config/iterm2.zsh
+++ config/iterm2.zsh
@@ -13,7 +13,13 @@ function iterm2_print_user_vars() {
   # Node version
   if (( $+commands[node] )); then
     node_version=$(node -e 'console.log(process.version.slice(1))' 2>/dev/null)
-    [[ $node_version == "" ]] || iterm2_set_user_var node_version "$node_version"
+    [[ $node_version == "" ]] || iterm2_set_user_var node_version "Node $node_version"
+  fi
+
+  # Rust version
+  if (( $+commands[rustc] )); then
+    rust_version=$(rustc --version 2>/dev/null | head -n1 | cut -d' ' -f2)
+    [[ $rust_version == "" ]] || iterm2_set_user_var rust_version "Rust $rust_version"
   fi
 
   # Disable custom rprompt
