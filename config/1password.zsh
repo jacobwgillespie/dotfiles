6e3e606336dd973ce7b98389ae7ee17488f92623
Jacob Gillespie <jacobwgillespie@gmail.com>
2022-10-23 00:52:05 +0100

    Add op completion

--- config/1password.zsh
+++ config/1password.zsh
@@ -1,3 +1,8 @@
 if [[ -e ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ]]; then
   export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
 fi
+
+if (( $+commands[op] )); then
+  eval "$(op completion zsh)"
+  compdef _op op
+fi
