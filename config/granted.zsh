d79fa34b8ca37bee618b7c59c76f801d349bc678
Jacob Gillespie <jacobwgillespie@gmail.com>
2023-11-22 13:38:08 +0000

    Install Common Fate

---
+++ config/granted.zsh
@@ -0,0 +1,10 @@
+alias assume=". assume"
+
+# Installation
+#
+# ln -sF '/Applications/Common Fate.app/Contents/Resources/CLI/assumego' ./bin/assumego
+# ln -sF '/Applications/Common Fate.app/Contents/Resources/CLI/assumego' ./bin/granted
+# ln -sF '/Applications/Common Fate.app/Contents/Resources/CLI/assume' ./bin/assume
+#
+# granted settings set -s DefaultBrowser -v COMMON_FATE
+# granted settings set -s CustomBrowserPath -v "/Applications/Common Fate.app/Contents/MacOS/Common Fate"
